6e5752c98f8d311437a1247dfff3e0f09f6103c0
stevannohero <stevannohero@gmail.com>
2018-03-26 21:57:29 +0700

    windows get hehe

--- Car/car.cpp
+++ Car/car.cpp
@@ -13,10 +13,23 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "car", NULL, NULL);
+
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (window == NULL) {
+		cout << "WINDOW'S FAILED" << endl;
+		glfwTerminate();
+		return 1;
+	} else {
+		cout << "WINDOW'S OK" << endl;
+	}
+
+	glfwMakeContextCurrent(window);
+
 
 
 	return 0;
