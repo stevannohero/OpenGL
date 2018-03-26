f58f8a57d379558876dbe167b0c81b62479984e1
stevannohero <stevannohero@gmail.com>
2018-03-26 21:48:37 +0700

    add get window size

--- Car/car.cpp
+++ Car/car.cpp
@@ -2,8 +2,22 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+using namespace std;
+
+const GLuint WIDTH = 1280, HEIGHT = 800;
+
 int main() {
-	
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "car", NULL, NULL);
+
 
 	return 0;
 }
